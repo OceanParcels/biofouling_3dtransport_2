bb38ed5e22ff395d8d5c278628947d1bc776a3aa
Reint Fischer <rfischer@int2.bullx>
2021-06-29 10:55:32 +0200

    Fix divide by aa bug

--- Simulation/regional_cartesius.sh
+++ Simulation/regional_cartesius.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-#SBATCH -t 15:00:00
+#SBATCH -t 24:00:00
 #SBATCH -p normal 
 #SBATCH -N 1 --ntasks-per-node=2
-#SBATCH --job-name EqPac_d
-#SBATCH --output EqPac_d
+#SBATCH --job-name SO
+#SBATCH --output SO
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=end
 #SBATCH --mail-type=fail
 #SBATCH --mail-user=r.p.b.fischer@uu.nl
 echo 'Initiating run'
-srun python regional_Kooi+NEMO_3D.py -mon=10 -yr=2003 -region='EqPac' -mixing='markov_0_KPP_ceiling_tides' -biofouling='MEDUSA_detritus' -system='cartesius' -no_advection='True'
+srun python regional_Kooi+NEMO_3D.py -mon=10 -yr=2003 -region='SO' -mixing='markov_0_KPP_ceiling_tides' -biofouling='MEDUSA' -system='cartesius' -no_advection='True'
 echo 'Finished computation.'
-
-#cd ${HOME}/biofouling_3dtransport_2/Simulation/
